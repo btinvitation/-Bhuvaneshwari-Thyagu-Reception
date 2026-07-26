34ae6182e41ece7365a6add2121fe352fbd4d782
btinvitation <ngkj5zgdh4@privaterelay.appleid.com>
2026-07-26 12:20:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,20 @@ if (envelope) {
     });
 
 }
+
+// Animate every section while scrolling
+
+gsap.utils.toArray("section").forEach((section) => {
+
+    gsap.from(section, {
+        opacity: 0,
+        y: 80,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%"
+        }
+    });
+
+});
